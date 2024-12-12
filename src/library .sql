fe7c806533875af240009df0de79bd065e9e9f1b
Deeksha1702 <116424631+Deeksha1702@users.noreply.github.com>
2024-12-12 20:30:24 +0530

    Update library .sql

--- src/library .sql
+++ src/library .sql
@@ -1,16 +1,14 @@
--- Create the database
-CREATE DATABASE Library;
 
--- Switch to the new database
+CREATE DATABASE Library;
 USE Library;
 
--- Create the Category table
 CREATE TABLE Category (
     id INT PRIMARY KEY AUTO_INCREMENT,
     catname VARCHAR(255),
     status VARCHAR(255)
 );
 SELECT * FROM Category;
+
 CREATE TABLE author (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -29,18 +27,22 @@ CREATE TABLE publisher (
     PRIMARY KEY (id)
 );
 SELECT * FROM publisher;
+
 CREATE TABLE book (
     id INT PRIMARY KEY AUTO_INCREMENT,
     bname VARCHAR(255) NOT NULL,
-    author INT,
-    publisher INT,
-    contents VARCHAR(255),
+    author INT,  
+    publisher INT,  
     pages INT,
-    edition INT,
-    category VARCHAR(50)
+    edition VARCHAR(50),  
+    category INT,  
+    FOREIGN KEY (author) REFERENCES author(id),  
+    FOREIGN KEY (publisher) REFERENCES publisher(id),  
+    FOREIGN KEY (category) REFERENCES category(id)  
 );
+
 SELECT * FROM book;
-select b.id,b.bname,c.catname,a.name,p.name,b.contents,b.pages,b.edition from book b JOIN category c on b.category =c.id  JOIN author a on b.author = a.id JOIN publisher p on b.publisher = p.id;
+
 
 CREATE TABLE member (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -49,27 +51,41 @@ CREATE TABLE member (
     phone VARCHAR(20)
 );
 SELECT * FROM member;
+
+
 CREATE TABLE ibook (
     id INT NOT NULL AUTO_INCREMENT,
     memberid INT NOT NULL,
-    bookid INT NOT NULL,
+    bookid INT NOT NULL, 
     issuedate DATE NOT NULL,
-    returnid INT NOT NULL,
     returndate DATE,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (memberid) REFERENCES member(id),
+    FOREIGN KEY (bookid) REFERENCES book(id)
 );
+
 SELECT * FROM ibook;
-select ib.id,m.name,b.bname,ib.issuedate,ib.returndate from ibook ib JOIN member m ON ib.memberid = m.id JOIN book b ON ib.bookid =b.id;
+
+
 CREATE TABLE returnbook (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    mid INT,
-    mname VARCHAR(255),
-    bname VARCHAR(255),
-    returndate VARCHAR(255),
-    elp INT
+    mid INT NOT NULL,  
+    mname VARCHAR(255) NOT NULL, 
+    bname VARCHAR(255) NOT NULL,  
+    returndate VARCHAR(255) NOT NULL,  
+    elp INT DEFAULT 0,  
+    fine INT DEFAULT 0 
+    
 );
+
 SELECT * FROM returnbook;
+
+
+select b.id,b.bname,c.catname,a.name,p.name,b.contents,b.pages,b.edition from book b JOIN category c on b.category =c.id  JOIN author a on b.author = a.id JOIN publisher p on b.publisher = p.id;
+select ib.id,m.name,b.bname,ib.issuedate,ib.returndate from ibook ib JOIN member m ON ib.memberid = m.id JOIN book b ON ib.bookid =b.id;
 select m.name,b.bname, ib.returndate,DATEDIFF(now(),ib.returndate) as elap from ibook ib JOIN member m ON ib.memberid = m.id JOIN book b ON ib.bookid=b.id ;
+
+
 
 
 
